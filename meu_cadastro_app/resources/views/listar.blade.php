6e9aab7446e99f6d1cd656d7a844ca74024c7e76
Verdinho-IMA <leonardo22rami@gmail.com>
2024-03-13 09:52:42 -0300

    first commit

---
+++ meu_cadastro_app/resources/views/listar.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Agendamento</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+
+  <body>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<!-- código do navbar -->
+<center>
+    <nav class="navbar navbar-expand-lg   bg-primary" data-bs-theme="dark"> 
+  <div class="container-fluid">
+ 
+    <a class="navbar-brand" href="#">Sistema WEB</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+ 
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+        <a class="nav-link" aria-current="page" href="#">Consultar</a>
+      </div>
+    </div>
+ 
+  </div>
+</nav>
+<!-- fim do código do navbar -->
+<br><br>
+
+ <h2> Cadastrar - Agendamento de Potenciais Clientes </h2>
+ <br>
+ <h4> Sistema utilizado para agendamentos de serviços </h4>
+
+<br><br>
+
+
+<div class="container text-center" style="background-color: #e3f2fd;">
+
+  <div class="row">
+    <div class="col"> Nome </div>
+    <div class="col"> Endereço </div>
+    <div class="col"> Bairro </div>
+  </div>
+
+</div>
+<br>
+<div class="container text-center" style="background-color: #e3f2fd;">
+
+<div class="row">
+    <div class="col"> CEP </div>
+    <div class="col"> Cidade </div>
+    <div class="col"> Estado </div>
+    <div class="col"> Ação </div>
+    
+</div>
+
+</center>
+<script src="/bootstrap-5.3.3-dist/js/boostrap.js"></script>
+
+  </body>
+
+</html>
